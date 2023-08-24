7ba843180649dc6d209a541ef3dc8ee2e6d7143e
LitavrinaAA <a.a.litavrina@google.com>
2023-08-24 11:36:31 +0300

    Диапозон значений по номеру четверти

---
+++ ex5/Program.cs
@@ -0,0 +1,22 @@
+﻿// по заданной четверти вывести диапозон допустимых значений
+
+int InputNum(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void NumbersInQ(int q1234){
+    if (q1234 == 1)
+         System.Console.Write("x > 0, y > 0");
+    else if (q1234 == 2)
+         System.Console.Write("x < 0, y > 0"); 
+    else if (q1234 == 3)
+        System.Console.Write("x < 0, y < 0"); 
+    else if (q1234 == 4)
+        System.Console.Write("x > 0, y < 0"); 
+    else   
+        System.Console.Write ("Введено не корректное значение.");
+}
+
+NumbersInQ(InputNum("Введите номер четверти: "));
