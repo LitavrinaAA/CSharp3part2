13d115a7288590bdc55a183f20bc3a18aa0e0069
LitavrinaAA <a.a.litavrina@google.com>
2023-08-24 10:34:22 +0300

    Задача однокурсника через строку

---
+++ ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if ( num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+String numToStr = num.ToString();
+char secondNum = numToStr[2];
+
+Console.WriteLine($"Вторым числом в {num} будет число {secondNum}");
