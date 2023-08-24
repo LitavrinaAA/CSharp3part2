f74c76247fa647b8d6e52b58963b561ff2412a48
LitavrinaAA <a.a.litavrina@google.com>
2023-08-24 10:08:31 +0300

    Третье число из 3-х значного

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//using System.Diagnostics;
+//Console.WriteLine("Третьей цифры нет");
+Console.WriteLine("Hello");
+
+int InputNum(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+bool Check(int num)
+{
+    return num >= 100;
+}
+
+int ThirdNum(int num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    return num % 10;
+}
+int number = InputNum("Введите число: ");
+if (Check(number))
+{
+    int thirdNum = ThirdNum(number);
+    Console.WriteLine($"Третья цифра числа {number} равна {thirdNum}");
+}else{
+     Console.WriteLine("Третьей цифры нет");
+}
