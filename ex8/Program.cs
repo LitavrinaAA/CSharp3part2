0956070d278713301b8f40a55863790a1ae982c8
LitavrinaAA <a.a.litavrina@google.com>
2023-08-24 13:44:15 +0300

    Квадраты от 1 до N

---
+++ ex8/Program.cs
@@ -0,0 +1,21 @@
+﻿// принимает число, выдает квадраты чисел
+// 5 -> 1, 4 , 9, 
+
+int InputNum(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+string SqFrom1ToN(int num)
+{
+    int i = 1;
+    string res = String.Empty;
+    while (i <= num)
+    {
+        res += " " + Math.Pow(i, 2);
+        i++;
+    }
+    return res;
+}
+System.Console.Write(SqFrom1ToN(InputNum("введите число: ")));
