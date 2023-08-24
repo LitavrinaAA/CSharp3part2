f32a7f6621f59a3e72ccc744700b2b5c16f8e6c0
LitavrinaAA <a.a.litavrina@google.com>
2023-08-24 10:24:17 +0300

    Еще вариант Третье число из 3-х значного

---
+++ ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int ThirdNum(int num)
+{
+    int thirdNum = num % 10;
+}
+
+int ConvertNum(int number)
+{
+    while (number >= 1000) number = number/10;
+    return number;
+}
+int num = InputNum("Введите любое целое число:");
+int result = num;
+
+if (Math.Abs(result) >= 100)
+{
+    int thirdNum = ThirdNum(ConvertNumber(Math.Abs(result)));
+    Console.WriteLine($"Третья цифра в числе {num} это {thirdNum}");
+} else Console.WriteLine($"Третьей цифры в числе {num} нет");
